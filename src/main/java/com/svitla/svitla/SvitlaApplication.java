4c20d0bd5764ec84aad6bf9d77e916d58d6fe143
FedericoFortuna <fedekpg17@gmail.com>
2023-10-05 12:35:16 -0300

    first commit

---
+++ src/main/java/com/svitla/svitla/SvitlaApplication.java
@@ -0,0 +1,13 @@
+package com.svitla.svitla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvitlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvitlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svitla/svitla/SvitlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svitla.svitla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvitlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
